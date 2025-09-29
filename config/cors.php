338785b27d52cc003889ddb2348bd3bdcbcb315e
Vinicius Cesar <128622082+Viniciuscesarr@users.noreply.github.com>
2025-09-29 14:29:45 -0300

    Atualizar o cors.php

--- config/cors.php
+++ config/cors.php
@@ -1,24 +1,23 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
-use Illuminate\Foundation\Configuration\Middleware;
-
-return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        // Adicione estas linhas:
-        $middleware->validateCsrfTokens(except: [
-            'api/*',
-        ]);
-        
-        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'],
+    
+    'allowed_methods' => ['*'],
+    
+    'allowed_origins' => [
+        'https://seu-frontend.vercel.app',
+        'http://localhost:3000',
+        'http://localhost:5173' // ou a porta do seu frontend local
+    ],
+    
+    'allowed_origins_patterns' => [],
+    
+    'allowed_headers' => ['*'],
+    
+    'exposed_headers' => [],
+    
+    'max_age' => 0,
+    
+    'supports_credentials' => true,
+];
