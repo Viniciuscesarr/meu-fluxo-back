f682533e88d48637fd9a50f7444df1b08a711984
Vinicius Cesar <128622082+Viniciuscesarr@users.noreply.github.com>
2025-09-29 14:32:09 -0300

    Atualizar o app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,15 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        // Configure CORS
+        $middleware->validateCsrfTokens(except: [
+            'api/*',
+        ]);
+        
+        // Adicione o middleware CORS
+        $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
