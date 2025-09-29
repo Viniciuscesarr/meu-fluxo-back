47c711f6a60e0c7f011147314a487ce189f78e14
Vinicius Cesar <128622082+Viniciuscesarr@users.noreply.github.com>
2025-09-29 14:40:18 -0300

    Atualizar o sanctum.php

--- config/sanctum.php
+++ config/sanctum.php
@@ -1,12 +1,10 @@
-
 <?php
 
 use Laravel\Sanctum\Sanctum;
 
 return [
-
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,meu-fluxo-nine.vercel.app,https://meu-fluxo-nine.vercel.app'
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,meu-fluxo-nine.vercel.app'
     )),
 
     'guard' => ['web'],
@@ -20,5 +18,4 @@ return [
         'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
         'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
     ],
-
 ];
